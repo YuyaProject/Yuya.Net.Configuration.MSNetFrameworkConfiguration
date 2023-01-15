4e333f6c2b03dcce2de6f16a8ca5bf15c162e5e9
Alper Konuralp <alper@konuralp.gen.tr>
2023-01-15 15:46:35 +0300

    "Alper ile Püf Noktası" - Episode 9 codes.

---
+++ src/DemoProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+using Yuya.Net.Configuration.MSNetFrameworkConfiguration;
+
+namespace DemoProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var conf = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .AddMSNetFrameworkConfiguration()
+                .Build();
+
+            Console.WriteLine(conf.GetValue<string>("Demo1"));
+        }
+    }
+}
